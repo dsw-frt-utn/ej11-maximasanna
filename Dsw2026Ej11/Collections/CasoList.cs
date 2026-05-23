c308591e67e86103590086b461e21d7773053e72
Maxima Sanna <maximasanna@gmail.com>
2026-05-22 23:08:06 -0300

    Resuelto CasoLinq

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,16 +1,4 @@
 ﻿using Dsw2026Ej11.Domain;
-
-namespace Dsw2026Ej11.Collections;
-
-//Crear un campo que represente una lista de alumnos (List<>)
-//Incluir un método para agregar alumnos a la lista
-//Incluir un método para retornar la lista
-//Incluir un método para buscar un alumno por nombre
-//Incluir un método para eliminar un alumno (debe recibir un alumno)
-//Incluir un método para eliminar un alumno en una determinada posición de la lista
-public class CasoList
-{
-    using Dsw2026Ej11.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -57,4 +45,4 @@ namespace Dsw2026Ej11.Collections
         }
     }
 }
-}
+
